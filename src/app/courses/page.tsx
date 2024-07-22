5acb1aa6ff3558e4663eaf45a46ee5f9c656e648
khairibzd <khairibouzid95@gmail.com>
2024-07-22 20:09:22 +0100

    adding pagination && filter features to the course page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import prisma from "@/lib/prisma";
+import { getAllCourses } from "@/actions/courses";
+import AllCourses from "@/components/AllCourses";
+import PaginationComponent from "@/components/PaginationComponent";
+import { Suspense } from "react";
+import CoursesSkeleton from "@/components/skeletons/CoursesSkeleton";
+import { Course } from "@prisma/client";
+import CourseFilter from "./course-filter";
 
-type Props = {}
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    page?: string;
+    levels?: string;
+  };
+}) {
+  function stringToArray(str: string): string[] {
+    return str.split(",").map((element) => element.trim());
+  }
 
-const page = (props: Props) => {
+  const levels = searchParams?.levels ? stringToArray(searchParams.levels) : [];
+  const page = parseInt(searchParams?.page || "1");
+  const pageSize = 4;
+
+  const { data: courses } = await getAllCourses({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+    levels,
+  });
+
+  const courseCount = await prisma.course.count();
   return (
-    <div>this is the courses page</div>
-  )
+    <section>
+      <h1 className="text-center text-4xl mb-10">Explore All Courses</h1>
+      <CourseFilter />
+      <div className="flex flex-col gap-3 m-10 mx-36">
+        <Suspense fallback={<CoursesSkeleton />}>
+          <AllCourses
+            //@ts-ignore
+            courses={courses}
+          />
+        </Suspense>
+        <PaginationComponent
+          pageSize={pageSize}
+          currentPage={page}
+          itemCount={courseCount}
+        />
+      </div>
+    </section>
+  );
 }
---
+++ src/components/PaginationComponent.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationLink } from "./ui/pagination";
+
+type Props = {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+};
+
+const PaginationComponent = ({ itemCount, pageSize, currentPage }: Props) => {
+  const {replace} = useRouter();
+  const searchParams = useSearchParams();
+
+  const displayPageCount = 3;
+  const generatePaginationLinks = () => {
+    const paginationLinks = [];
+    const leftEllipsis = currentPage > 2;
+    const rightEllipsis = currentPage < pageCount - 1;
+
+    for (let i = 1; i <= pageCount; i++) {
+      if (
+        i === 1 ||
+        i === pageCount ||
+        (i >= currentPage - 1 && i <= currentPage + 1)
+      ) {
+        paginationLinks.push(
+          <PaginationLink
+            key={i}
+            onClick={() => changePage(i)}
+            isActive={currentPage === i}
+          >
+            {i}
+          </PaginationLink>
+        );
+      }
+    }
+
+    if (leftEllipsis) {
+      paginationLinks.splice(1, 0, <PaginationEllipsis key="left" />);
+    }
+    if (rightEllipsis) {
+      paginationLinks.splice(
+        paginationLinks.length - 1,
+        0,
+        <PaginationEllipsis key="right" />
+      );
+    }
+
+    return paginationLinks;
+  };
+
+  const pageCount = Math.ceil(itemCount / pageSize);
+  if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    replace("?" + params.toString());
+  };
+  return (
+    <Pagination>
+      <PaginationContent className=" *:cursor-pointer">
+        <Button
+          variant="ghost"
+          disabled={currentPage <= 1}
+          onClick={() => changePage(currentPage - 1)}
+          className="group"
+        >
+          <ChevronLeft className="group-hover:-translate-x-1 transition-all duration-300 delay-150" />{" "}
+          Previous
+        </Button>
+        {generatePaginationLinks()}
+        <Button
+          variant="ghost"
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(currentPage + 1)}
+          className="group"
+        >
+          Next{" "}
+          <ChevronRight className="group-hover:translate-x-1 transition-all duration-300 delay-150" />
+        </Button>
+      </PaginationContent>
+    </Pagination>
+  );
+};
+
+export default PaginationComponent;
