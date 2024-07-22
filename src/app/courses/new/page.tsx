39d76ba0367190e8a5fbe714071f830590fec6d8
khairibzd <khairibouzid95@gmail.com>
2024-07-22 20:07:19 +0100

    adding add course page with zod validation

--- src/app/courses/new/page.tsx
+++ src/app/courses/new/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import AddCourseForm from "@/components/forms/AddCourseForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-type Props = {}
-
-const page = (props: Props) => {
+const Page = () => {
   return (
-    <div>This is teh new course page</div>
-  )
-}
+<Card className="max-w-lg mx-auto">
+  <CardHeader className="space-y-1 text-center">
+    <CardTitle className="text-2xl">Add Course</CardTitle>
+    <CardDescription>Add a new course to the list</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <AddCourseForm />
+  </CardContent>
+</Card>
+  );
+};
+
+export default Page;
